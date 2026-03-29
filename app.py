c35d5ea8414dee1942ca1e956249a3455dfb3264
JermTheWorm12 <thepurpletophat13@gmail.com>
2026-03-29 14:09:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db():
   if not DATABASE_URL:
-        raise RuntimeError("DATABASE_URL is not set")
-    return psycopg2.connect(DATABASE_URL)
+    raise RuntimeError("DATABASE_URL is not set")
+  return psycopg2.connect(DATABASE_URL)
 
 INITIAL_FILE_DETAILS = {
     "Finch": """[ACCESS: O.DEPTHS // PERSONNEL FILE: FINCH]
